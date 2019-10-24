01f14873ab8788a95946694f7a03ba022710ec2a
ahenriquez <ahenriquez@eficent.com>
2019-10-24 16:58:41 +0200

    [MIG]project_bid to v12

--- project_bid/__manifest__.py
+++ project_bid/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-19 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Project Bid",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "www.eficent.com",
     "license": "LGPL-3",
     "category": "Generic Modules/Projects & Services",
-    "depends": ["project"],
+    "depends": ["project", "product"],
     "data": [
         "security/project_bid_security.xml",
         "view/project_bid_template_view.xml",
@@ -17,5 +16,5 @@
         "security/ir.model.access.csv",
         "data/project_bid_data.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
